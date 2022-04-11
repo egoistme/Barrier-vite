64960b109b1dd67fac5a3abc8fcdc58cb18f7323
egoistme <390068842@qq.com>
2022-04-11 10:36:41 +0800

    添加components文件夹

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,29 @@
+module.exports = {
+  title:"Barrier",
+  themeConfig:{
+    nav:[
+      {
+        text:'web前端',
+        link:'/',
+        items:[
+          {
+            text:'Javascript',
+            link:'/javascript'
+          },
+          {
+            text:'Flutter',
+            link:'/flutter'
+          },
+          {
+            text:'Animate',
+            link:'/animate'
+          },
+          {
+            
+          }
+        ]
+      },
+
+    ]
+  } 
+} 
